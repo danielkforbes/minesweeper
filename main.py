fc382e1c24331bf4c8886030f11ce8c464fc927a
danielkforbes <54292613+danielkforbes@users.noreply.github.com>
2023-01-23 17:16:15 -0500

    Update main.py
    Created introduction prompt to setup Game class

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #Main.py for Python Minesweeper Project
+print("Welcome to MinesweePyr!")
+
+class Game:
+  pass
+
+diff_list = ["e", "m", "h", "easy", "medium", "hard"]
+print("Choose your difficulty:")
+difficulty = input("Easy (E), Medium (M), or Hard (H)\n")
+  if difficulty.lower() not in diff_list:
+    print("Invalid entry. Game Over before it started.")
+  else:
+    sweep = Game(difficulty)
