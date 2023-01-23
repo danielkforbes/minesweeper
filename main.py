ce91684f7c09f19b0e4ce0dfa606785410c75820
danielkforbes <54292613+danielkforbes@users.noreply.github.com>
2023-01-23 17:25:04 -0500

    Update main.py
    Defined Game class initialization for rows and columns.

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 #Main.py for Python Minesweeper Project
 print("Welcome to MinesweePyr!")
+diff_list = ["e", "m", "h", "easy", "medium", "hard"]
+gameOver = False
 
 class Game:
-  pass
+  def __init__(self, difficulty):
+    if difficulty == "h" or difficulty == "hard":
+      self.rows, self.columns = 20
+    elif difficulty == "m" or difficulty == "medium":
+      self.rows, self.columns = 10
+    else:
+      self.rows, self.columns = 5
 
-diff_list = ["e", "m", "h", "easy", "medium", "hard"]
 print("Choose your difficulty:")
 difficulty = input("Easy (E), Medium (M), or Hard (H)\n")
   if difficulty.lower() not in diff_list:
     print("Invalid entry. Game Over before it started.")
+    gameOver = True
   else:
     sweep = Game(difficulty)
+
+#GameLoop will go here
